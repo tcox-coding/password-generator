a427268e6d389a2d1c53feaec00af9918d30ae15
Taylor Cox <taylor@coxintl.com>
2025-03-19 04:52:32 -0400

    whoops get rid of that lil mistake

--- password-generator.py
+++ password-generator.py
@@ -5,7 +5,5 @@ length = input('Enter password length: ')
 
 possible_chars = string.ascii_letters + './1234567890!@#$%^&*()'
 
-print(secrets.choice(possible_chars))
-
 for i in range(int(length)):
     print(random.choice(possible_chars), end='')
